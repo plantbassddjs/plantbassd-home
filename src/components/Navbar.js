002f5c21eddadbe36bc87fbb2aeca029f9618cc9
Michael Savage <msavage3@jaguarlandrover.com>
2021-07-15 15:51:08 +0100

    transparent navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,42 +1,24 @@
 import React from 'react';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
 import InputBase from '@material-ui/core/InputBase';
 import { fade, makeStyles } from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu';
-import Button from '@material-ui/core/Button';
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 import SearchIcon from '@material-ui/icons/Search';
 
 import logo from '../assets/img/logo_circle.png';
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        flexGrow: 1,
-        marginLeft: theme.spacing(2),
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        [theme.breakpoints.up('sm')]: {
-        display: 'block',
-        },
-    },
     search: {
         position: 'relative',
         borderRadius: theme.shape.borderRadius,
-        backgroundColor: fade(theme.palette.common.white, 0.15),
-        '&:hover': {
-        backgroundColor: fade(theme.palette.common.white, 0.25),
+        backgroundColor: fade(theme.palette.common.white, 0.90),'&:hover': {
+            backgroundColor: fade(theme.palette.common.white, 0.80),
         },
         marginLeft: 0,
         width: '100%',
         [theme.breakpoints.up('sm')]: {
-        marginLeft: theme.spacing(1),
-        width: 'auto',
+            marginLeft: theme.spacing(1),
+            width: 'auto',
         },
     },
     searchIcon: {
@@ -58,11 +40,11 @@ const useStyles = makeStyles((theme) => ({
         transition: theme.transitions.create('width'),
         width: '100%',
         [theme.breakpoints.up('sm')]: {
-        width: '12ch',
-        '&:focus': {
-            width: '20ch',
+            width: '12ch', '&:focus': {
+                width: '20ch',
+            },
         },
-        },
+        fontSize: '1.4em'
     },
 }));
 
@@ -70,37 +52,39 @@ export default function SearchAppBar() {
     const classes = useStyles();
 
     return (
-        <div className={classes.root}>
-        <AppBar position="static">
-            <Toolbar>
+        <Navbar collapseOnSelect expand="lg" className="bg-nav-colour">
 
-                <img src={logo} className="img-logo p-2" alt="plant bassd logo" />
+            <Navbar.Brand href="#home">
+                <img
+                    alt="plant bassd logo" src={logo}
+                    width="110" height="110"
+                    className="d-inline-block align-center"
+                />{' '}
+            </Navbar.Brand>
 
-                <div className={classes.title}>
-                    <Button className="m-2" variant="contained">Home</Button>
-                    <Button className="m-2" variant="contained">Contact</Button>
-                </div>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse id="responsive-navbar-nav">
 
-                <Typography className={classes.menuButton} variant="h4" align="center">
-                    Plant Bass`d DJs
-                </Typography>
+                <Nav className="me-auto nav-text-size">
+                    <Nav.Link href="#home" className="pr-4">Home</Nav.Link>
+                    <Nav.Link href="#home" className="pr-4">Contact</Nav.Link>
+                </Nav>
 
-                <div className={classes.search}>
-                    <div className={classes.searchIcon}>
-                        <SearchIcon />
+                <Nav className="ml-auto">
+                    <div className={classes.search}>
+                        <div className={classes.searchIcon}>
+                            <SearchIcon />
+                        </div>
+
+                        <InputBase placeholder="Search…"
+                            classes={{
+                                root: classes.inputRoot,
+                                input: classes.inputInput,
+                            }}
+                            inputProps={{ 'aria-label': 'search' }}/>
                     </div>
-
-                    <InputBase
-                        placeholder="Search…"
-                        classes={{
-                            root: classes.inputRoot,
-                            input: classes.inputInput,
-                        }}
-                        inputProps={{ 'aria-label': 'search' }}
-                    />
-                </div>
-            </Toolbar>
-        </AppBar>
-        </div>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 }
